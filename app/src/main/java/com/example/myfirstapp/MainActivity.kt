6ca6dae8dc52f60c3cb6d74d8fd4bf15fafe4ef2
Ranjit Das <ranjitda@creative.local>
2023-11-23 15:40:32 +0530

    Commit kali Haria lekhiki

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             MyFirstAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Ranjit Bro")
+                    Greeting("Haria Bro")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyFirstAppTheme {
-        Greeting("Ranjit")
+        Greeting("haria")
     }
 }
